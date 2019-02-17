8f096a23b2c166ed41fb6f68a59c9c0592eccb95
Александр Смоляк <aleksandr.smoliak@lenvendo.ru>
2019-02-17 17:59:11 +0300

    Запись и рефакторинг нового теска. Создание нового класса и использование этого класса для передачи данных во вспомогательный метод.

---
+++ contact.py
@@ -0,0 +1,27 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company_name, company_address,
+                        home_phone, mobile_phone, work_phone, fax_phone, email, email2, email3, homepage,
+                        birthday_selected, birthmont_selected, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company_name = company_name
+        self.company_address = company_address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax_phone = fax_phone
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birthday_selected = birthday_selected
+        self.birthmont_selected = birthmont_selected
+        self.byear = byear
+
+
+
+
+
