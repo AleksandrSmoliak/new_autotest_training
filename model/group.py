5062e7f88fe2eeb7d9a26106821778419c414bc5
Александр Смоляк <aleksandr.smoliak@lenvendo.ru>
2019-03-17 15:21:57 +0300

    1. Добавлена проверка сравнение элементов списка групп (не длина, а именно элементов) 2. В класс объекта групп добавлены функции визуализации вывода объектов на консоль и функция метода сравнения. Т.е. по каким полям происходит сравнение объектов данного класса.

--- model/group.py
+++ model/group.py
@@ -4,3 +4,11 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    # Определяем вид объекта при выводе на консоль
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    # Определяем операцию сравнения объектов данного класса (каким образом сравниваем)
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -13,5 +13,9 @@ def test_delete_first_group(app):
     new_groups = app.group.get_group_list()
     # Проверяем что произошло удаление новой группы сравнивая их длину
     assert len(old_groups) - 1 == len(new_groups)
+    # Удаляем первый элемент из старого списка групп
+    old_groups[0:1] = []
+    # Проверяем получившиеся списки групп
+    assert old_groups == new_groups
     # Возврат на страницу со списком групп
     app.session.return_home_page()
