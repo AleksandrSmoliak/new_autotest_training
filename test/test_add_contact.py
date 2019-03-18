0c6613b54825eee5ec0b4a98ca979d38c2e25166
AleksandrSmoliak <crucis.spb@gmail.com>
2019-03-18 15:10:53 +0300

    Добавлены проверки для сравнения длины списков в тестах добавления и модификации контактов.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,6 +3,8 @@ from model.contact import Contact
 
     
 def test_test_add_contact(app):
+    # Получаем старый список контактов
+    old_contact = app.contact.get_contact_list()
     # Добавление нового контакта
     app.contact.create(Contact(firstname="Александр", middlename="Смоляк", lastname="Владимирович", nickname="crucis", title="Заголовок", company_name="Имя компании",
                                company_address="адрес компании", home_phone="9119999999", mobile_phone="9119999991", work_phone="9119999992", fax_phone="9119999993", email="as@as.ru",
@@ -10,7 +12,10 @@ def test_test_add_contact(app):
                                birthday_selected="//div[@id='content']/form/select[1]//option[12]",
                                birthmont_selected="//div[@id='content']/form/select[2]//option[7]", byear="1983",
                                home_address="Домашний адрес"))
-    
+    # Получаем новый список контактов
+    new_contact = app.contact.get_contact_list()
+    # Сравниваем длину списков до и после добавления
+    assert len(old_contact) + 1 == len(new_contact)
     # Вернуться на домашнюю страницу
     app.session.return_home_page()
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,8 +5,14 @@ def test_modify_contact_firstname(app):
     # Проверяем налисие контактов, если нет - создаем
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Тест"))
+    # Получаем старый список контактов
+    old_contact = app.contact.get_contact_list()
     # Выбираем и модифицируем первый найденный контакт
     app.contact.modify_first_contact(Contact(firstname="Владимир"))
+    # Получаем новый список контактов
+    new_contact = app.contact.get_contact_list()
+    # Сравниваем длину списков до и после модификации
+    assert len(old_contact) == len(new_contact)
     # Вернуться на домашнюю страницу
     app.session.return_home_page()
 
