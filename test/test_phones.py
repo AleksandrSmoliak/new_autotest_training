a14a36b6e91fe30a84b1c524f2d3205882d236b1
AleksandrSmoliak <crucis.spb@gmail.com>
2019-03-27 13:57:11 +0300

    Добавлены комментарии.

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,7 +1,10 @@
 # Тест сравнения контактов на главной странице и странице редактирования
 def test_phones_on_home_page(app):
+    # Получаем контакт с указанным индексом с главной страницы
     contact_from_home_page = app.contact.get_contact_list()[0]
+    # Получаем свойства контакта с указанным индексом со страницы редактирования
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    # Сравниваем значения полей с главной страницы и со страницы редактирования контакта
     assert contact_from_home_page.home_phone == contact_from_edit_page.home_phone
     assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
     assert contact_from_home_page.work_phone == contact_from_edit_page.work_phone
