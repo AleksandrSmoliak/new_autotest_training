a42807baa62d5bab65208a4684ed2bc56c63c5ae
AleksandrSmoliak <crucis.spb@gmail.com>
2019-04-03 14:36:48 +0300

    Добавлен геератор тестовых данных. Данные из которого используются для теста добавления групп.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+# Генератор случайных тестовых данных
+def random_string(prefix, maxlen):
+    # Присваиваем переменной наборы различных символов + пробел
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    # Возвращаем строку из склеенных символов. Список будет из случайных символов и случайной длинны с ограничением на
+    # значение maxlen
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+# Формируем 2 массива с данными. Первый пустой, а второй генерируется случайными данными, при этом генерируется
+# указанное количество раз, что позволяет создать нескольео наборов с данными.
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
+
+
+# Первая строка говорит, что тест парамметризован и указываем что передеаем и откуда в качестве данных
+# в параметре ids передается представление тестовых данных
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     # Получаем старый список групп
     old_groups = app.group.get_group_list()
-    # Локальная переменная для передачи параметра в создаваемую группу
-    group = Group(name="group name", header="group header", footer="group footer")
     # Создание новой группы
     app.group.create(group)
     # Проверяем что произошло добавление новой группы сравнивая их длину.
@@ -14,26 +35,6 @@ def test_add_group(app):
     assert len(old_groups) + 1 == app.group.count()
     # Получаем новый список групп
     new_groups = app.group.get_group_list()
-    # Добавляем создаваемую в прилжении группу в старую гуппу для дальнейшего сравнения
-    old_groups.append(group)
-    # Сравниваем старую и новую группу по содержанию. Сортировка по ИД вычесляемой в функции. Функция используется
-    # в качестве ключа.
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    # Возврат на страницу со списком групп
-    app.session.return_home_page()
-
-
-def test_empty_group(app):
-    # Получаем старый список групп
-    old_groups = app.group.get_group_list()
-    # Локальная переменная для передачи параметра в создаваемую группу
-    group = Group(name="", header="", footer="")
-    # Создание новой группы
-    app.group.create(group)
-    # Получаем новый список групп
-    new_groups = app.group.get_group_list()
-    # Проверяем что произошло добавление новой группы сравнивая их длину
-    assert len(old_groups) + 1 == len(new_groups)
     # Добавляем создаваемую в прилжении группу в старую гуппу для дальнейшего сравнения
     old_groups.append(group)
     # Сравниваем старую и новую группу по содержанию. Сортировка по ИД вычесляемой в функции. Функция используется
