1751b18c476f0fba9ede53262e9a979ee4dcede0
AleksandrSmoliak <crucis.spb@gmail.com>
2019-04-04 18:18:17 +0300

    Параметризация фикстуры. Добавлены опции запуска с указанием типа браузера и адреса приложения.

--- conftest.py
+++ conftest.py
@@ -1,20 +1,23 @@
-from fiture.application import Application
+from fixture.application import Application
 import pytest
 
 fixture = None  # Задаем глобальную переменную для определения валидности фикстуры
 
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture  # Объявление глобальной переменной
+    # Создаем переменную в которую передаем параметр с типом браузера
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseURL")
     # Проверяем, если фикстуры нету или она не валидна, тогда создаем ее.
     if fixture is None:
         # Создаем фикстуру (объект типа Application)
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             # Создаем фикстуру (объект типа Application)
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
             # Авторизация
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
@@ -24,8 +27,14 @@ def app():
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     # Возвращаем фиксуру
     return fixture
+
+
+# Функция позволяющая через параметр в консоли передавать тип браузера
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseURL", action="store", default="http://localhost/addressbook/")
---
+++ fixture/application.py
@@ -0,0 +1,39 @@
+#from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            # Если не удается определить переданный параметр выбрасываем исключение
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
+        self.base_url = base_url
+
+    # Проверякм валидность сессии по наличию урла
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
