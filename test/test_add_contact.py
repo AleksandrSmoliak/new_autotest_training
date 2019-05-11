e41efd8e40b6bdb5aaca98a04f146568f01b86ea
Александр Смоляк <aleksandr.smoliak@lenvendo.ru>
2019-05-11 16:58:38 +0300

    Дорботан тест добавления контактов с получением списков для сравнения из БД

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,15 +2,15 @@
 from model.contact import Contact
 
 
-def test_test_add_contact(app, json_contact):
+def test_test_add_contact(app, db, json_contact):
     # Присваиваем набор данных из параметра переменной используемой далее в тесте.
     contact = json_contact
-    # Получаем старый список контактов
-    old_contact = app.contact.get_contact_list()
+    # Получаем старый список контактов из БД
+    old_contact = db.get_contact_list()
     # Добавление нового контакта
     app.contact.create(contact)
-    # Получаем новый список контактов
-    new_contact = app.contact.get_contact_list()
+    # Получаем новый список контактов из БД
+    new_contact = db.get_contact_list()
     # Сравниваем длину списков до и после добавления
     assert len(old_contact) + 1 == app.contact.count()
     # Добавляем в старый список контактов создаваемый в приложении контакт
